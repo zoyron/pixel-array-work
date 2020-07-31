7afe100598e2073317be2ff4751373551fa1f69f
zoyron <sgrarora3@gmail.com>
2020-08-01 01:31:10 +0530

    random walker with image initiated

--- sketch.js
+++ sketch.js
@@ -1,26 +1,26 @@
 let img;
+let testImg;
 function preload() {
   img = loadImage("./e496x399.jpg");
 }
 
 function setup() {
+  testImg = createImage(img.width, img.height);
   createCanvas(700, 700);
   pixelDensity(1);
+  // img.loadPixels();
+  // testImg.loadPixels();
+  // for (let y = 0; y < testImg.pixels.length; y++) {
+  //   testImg.pixels[y] = img.pixels[y];
+  // }
+  testImg = img.get();
+  // testImg.updatePixels();
 }
 
 function draw() {
   background(10);
-  translate(width / 2, height / 2);
-  loadPixels();
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      let index = (y * width + x) * 4;
-      pixels[index + 0] = dist(y, x, x, y);
-      pixels[index + 1] = cos(x);
-      pixels[index + 2] = sin(x);
-      pixels[index + 3] = dist(y, x, x, y);
-    }
-  }
+  // image(img, 0, 0);
+  // translate(width / 2, height / 2);
 
-  updatePixels();
+  image(testImg, 0, 0);
 }
